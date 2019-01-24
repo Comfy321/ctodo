22562195ecb7f7fab8559ec69697d1a7902381e7
Comfy321 <lonelypyxel@yandex.com>
2019-01-23 17:50:19 -0800

    awk is wack

--- ctodo.sh
+++ ctodo.sh
@@ -1,35 +1,34 @@
 #ctodo - a comfy and very simple todo application.
-
+update()
+{
+		awk -i inplace '{$1 = FNR} 1' ~/todo
+}
 flag="${1}"
 case ${flag} in
 	-h)
 		echo "Help... Work in Progress.."
 		;;
 	-a) ADD="${2}"
-		update()
+		update
 		lines=$(wc -l < ~/todo)
 		lines=$((lines+1))
 		echo $lines
 		echo "$lines ${2}" >> ~/todo
 		;;
 	-c) DONE="${2}"
-		sed -i "${2}d" ~/todo
-		update()
+		sed -i "${2}d" ~/todo >> ~/todo
+		update
 		;;
 	-v) 
-		update()
+		update
 		cat ~/todo
 		;;
 	-u)
-		update()
+		update
 		;;
 	*)
 		echo "Flag not recognized. Use -h for help."
 		;;
 esac
 
-#Updates numbers of entries in todo list
-update()
-{
-		awk '{$1 = FNR} 1' ~/todo
-}
+
